672d848d16c15a69ce8bc3da2a6235000ae80b4e
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-09-05 13:29:11 +0200

    'Pomoc' and 'Kontakt' sections ready (without form to fill in). 'remake-box' done. All project ready for desktop.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navMobile = document.querySelector('.nav-mobile');
 const navMobileItems = document.querySelectorAll('.nav-mobile__item');
+const remakeBox = document.querySelector('.remake-box');
+const remakeBoxBtn = document.querySelector('.remake-box__btn');
+
 
 const displayMobNav = () => {
 	navMobile.classList.toggle('nav-mobile--active');
@@ -11,4 +14,15 @@ const displayMobNav = () => {
 		});
 	});
 };
+
+const showRemakeBox = () => {
+remakeBox.classList.add('box-active')
+}
+
+const hideRemakeBox = () => {
+	remakeBox.classList.remove('box-active')
+}
+
+document.addEventListener('DOMContentLoaded',showRemakeBox)
 burgerBtn.addEventListener('click', displayMobNav);
+remakeBoxBtn.addEventListener('click', hideRemakeBox);
